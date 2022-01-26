d1f9664c64a98fbb7bc930a68e1998bf0a68b6f0
dovanloidev <dvl.codervn@gmail.com>
2022-01-26 16:38:33 +0700

    custom bottom tab

--- src/navigation/BottomTab.tsx
+++ src/navigation/BottomTab.tsx
@@ -1,12 +1,31 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {theme} from 'native-base';
 import React from 'react';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import Todo from '../screens/Todo';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTab = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          let iconName = '';
+
+          if (route.name === 'Todo') {
+            iconName = 'list-alt';
+          } else {
+            iconName = 'list-alt';
+          }
+
+          return <FontAwesome5 name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: theme.colors.blue[500],
+        tabBarInactiveTintColor: theme.colors.gray[400],
+        headerShown: false,
+        tabBarVisible: true,
+      })}>
       <Tab.Screen name="Todo" component={Todo} />
     </Tab.Navigator>
   );
